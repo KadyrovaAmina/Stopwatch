4da52d91389a69b134c6388c0939c74d736547cd
Kadyrova Amina <99501882+KadyrovaAmina@users.noreply.github.com>
2022-07-05 01:26:20 +0600

    feat: add files

---
+++ Stopwatch/Stopwatch.js
@@ -0,0 +1,116 @@
+const hourElement = document.querySelector('.hour')
+const minuteElement = document.querySelector('.minute')
+const secondElement = document.querySelector('.second')
+const millisecondElement = document.querySelector('.millisecond')
+
+const startButton = document.querySelector('.start')
+const pauseButton = document.querySelector('.pause')
+const stopButton = document.querySelector('.stop')
+const newButton = document.querySelector('.new')
+
+startButton.addEventListener('click', () => {
+    clearInterval(interval)
+    interval = setInterval(startTimer, 10)
+})
+
+pauseButton.addEventListener('click', () => {
+    clearInterval(interval)
+})
+
+stopButton.addEventListener('click', () => {
+    clearInterval(interval)
+    clearFields()
+    disabledBtn()
+})
+
+newButton.addEventListener('click', () => {
+    clearInterval(interval)
+    counter++
+    const results = document.querySelector('.results')
+    const block = document.createElement('div')
+    block.classList.add('results__info')
+    block.innerText = `Result ${counter}: ${hour}:${minute}:${second}:${millisecond}`
+    results.append(block)
+    clearFields()
+    clearInterval(interval)
+    interval = setInterval(startTimer, 10)
+})
+
+let hour = 00,
+    minute = 00,
+    second = 00,
+    millisecond = 00,
+    interval,
+    counter = 0,
+    disabled = true
+
+function startTimer() {
+    millisecond++
+
+    //milliseconds
+    if(millisecond < 9){
+        millisecondElement.innerText = "0" + millisecond
+    }
+    if(millisecond > 9){
+        millisecondElement.innerText = millisecond
+    }
+    if(millisecond > 99){
+        second++
+        secondElement.innerText = "0" + second
+        millisecond = 0
+        millisecondElement.innerText = "0" + millisecond
+    }
+
+    //seconds
+    if(second < 9){
+        secondElement.innerText = "0" + second
+    }
+    if(second > 9){
+        secondElement.innerText = second
+    }
+    if(second > 59){
+        minute++
+        minuteElement.innerText = "0" + minute
+        second = 0
+        secondElement.innerText = "0" + second
+    }
+
+    //minutes
+    if(minute < 9){
+        minuteElement.innerText = "0" + minute
+    }
+    if(minute > 9){
+        minuteElement.innerText = minute
+    }
+    if(minute > 59){
+        hour++
+        hourElement.innerText = "0" + hour
+        minute = 0
+        minuteElement.innerText = "0" + minute
+    }
+
+    //hours
+    if(hour > 9){
+        minuteElement.innerText = hour
+    }
+
+    newButton.disabled = false
+}
+
+function clearFields(){
+    hour = 00
+    minute = 00
+    second = 00
+    millisecond = 00
+    hourElement.textContent = "00"
+    minuteElement.textContent = "00"
+    secondElement.textContent = "00"
+    millisecondElement.textContent = "00"
+}
+
+function disabledBtn(){
+    if(disabled){
+        newButton.disabled = true
+    }
+}
+disabledBtn()
